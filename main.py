c21310e0acabcd66c7213cd5d2b8fcd195be1e1d
Никита Баранов <n_baranov@corp.pm-m.uz>
2020-09-10 10:56:49 +0500

    First commit of a salary project files from office

---
+++ main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  9 17:28:01 2020
+
+@author: n_baranov
+"""
+
+import pandas as pd
+import glassdoor_scraper as gs
+
+path = r'C:\Users\user03\Documents\chromedriver\chromedriver.exe'
+slp_time=15
+
+df = gs.get_jobs('data scientist', 15, False, path, slp_time)
