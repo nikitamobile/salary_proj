03bb1c3f8e94b77323684aa5a86ddb38f405633d
Nikita <nikitamobie@gmail.com>
2020-09-13 19:37:48 +0500

    huynya

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ Created on Wed Sep  9 17:28:01 2020
 import pandas as pd
 import glassdoor_scraper as gs
 
-path = r'C:\Users\user03\Documents\chromedriver\chromedriver.exe'
+#path = r'C:\Users\user03\Documents\chromedriver\chromedriver.exe'
+path = r'D:\Niki\Data Science\salary_proj\salary_proj\chromedriver_win32\chromedriver.exe'
 slp_time=15
 
 df = gs.get_jobs('data scientist', 15, False, path, slp_time)
